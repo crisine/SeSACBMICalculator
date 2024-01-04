bd675499a9841a37c66c3ea5bc0d37ff740893bf
crisine <crisine@knou.ac.kr>
2024-01-05 03:20:33 +0900

    feat: 유저정보를 저장하고 불러오는 기능 및 삭제하는 기능 구현

--- SeSACBMICalculator/AlertType.swift
+++ SeSACBMICalculator/AlertType.swift
@@ -6,13 +6,16 @@
 //
 
 enum AlertType: String, CustomStringConvertible {
-    case invalidHeightInput = "유효하지 않은 키 입력",
+    case invalidNicknameLength = "유효하지 않은 길이의 닉네임",
+         invalidHeightInput = "유효하지 않은 키 입력",
          invalidWeightInput = "유효하지 않은 몸무게 입력",
          outOfHeightRange = "키 입력 범위 초과",
          outOfWeightRange = "몸무게 입력 범위 초과"
     
     var description: String {
         switch self {
+        case .invalidNicknameLength:
+            return "닉네임을 1~10자 사이로 입력해주세요"
         case .invalidHeightInput:
             return "정상적인 키 값을 숫자로 입력해주세요"
         case .invalidWeightInput:
--- SeSACBMICalculator/ViewController.swift
+++ SeSACBMICalculator/ViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nicknameTextField: UITextField!
     @IBOutlet weak var heightInputTextField: UITextField!
     @IBOutlet weak var weightInputTextField: UITextField!
     @IBOutlet weak var randomInputButton: UIButton!
     @IBOutlet weak var calculateBMIButton: UIButton!
+    @IBOutlet weak var resetUserInfoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         heightInputTextField.textContentType = .telephoneNumber
         weightInputTextField.textContentType = .telephoneNumber
+        loadUserInfo()
     }
     
     @IBAction func didCalculateBMIButtonTapped(_ sender: UIButton) {
@@ -48,18 +51,30 @@ class ViewController: UIViewController {
         
         let bmiValue = calculateBMIValue(height: height, weight: weight)
         
-        let alert = UIAlertController(title: "당신의 BMI 지수", message: "\(bmiValue)", preferredStyle: .alert)
+        let nickname = nicknameTextField.text == "" ? "아무개" : nicknameTextField.text!
+        
+        let alert = UIAlertController(title: "\(nickname)님의 BMI 지수", message: "\(bmiValue)", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인", style: .default)
         
         alert.addAction(okAction)
         present(alert, animated: true)
+        
+        saveUserInfo(nickname: nickname, height: height, weight: weight)
     }
     
     @IBAction func didRandomInputButtonTapped(_ sender: UIButton) {
-        heightInputTextField.text = String(Int.random(in: 40...300))
-        weightInputTextField.text = String(Int.random(in: 20...200))
+        heightInputTextField.text = String(format: "%.2f", Double.random(in: 40...300))
+        weightInputTextField.text = String(format: "%.2f", Double.random(in: 20...200))
     }
     
+    @IBAction func didEndEditingInNicknameTextField(_ sender: UITextField) {
+        let nicknameLength = sender.text?.count ?? 0
+        
+        if nicknameLength <= 0 || nicknameLength > 10 {
+            let alert = createAlert(alertType: .invalidNicknameLength)
+            present(alert, animated: true)
+        }
+    }
     
     @IBAction func didEndEditingInHeightTextField(_ sender: UITextField) {
         guard let height = getHeight() else {
@@ -91,6 +106,38 @@ class ViewController: UIViewController {
     
     func calculateBMIValue(height: Double, weight: Double) -> String {
         return String(format: "%.2f", weight / pow(height / 100.0, 2.0))
+    }
+    
+    func saveUserInfo(nickname: String, height: Double, weight: Double) {
+        // struct로 유저정보를 관리하면 좋을듯하나 시간상 차후로..
+        UserDefaults.standard.setValue(nickname, forKey: "nickname")
+        UserDefaults.standard.setValue(height, forKey: "height")
+        UserDefaults.standard.setValue(weight, forKey: "weight")
+    }
+    
+    // -> User 를 반환하는것이 좀 더 옳은 방안이라고 생각되나 이쪽도..
+    func loadUserInfo() {
+        if let nickname = UserDefaults.standard.string(forKey: "nickname") {
+            nicknameTextField.text = nickname
+        } else {
+            nicknameTextField.text = nil
+        }
+        
+        let height = UserDefaults.standard.double(forKey: "height")
+        heightInputTextField.text = height == 0 ? "" : String(format: "%.2f", height)
+        
+        let weight = UserDefaults.standard.double(forKey: "weight")
+        weightInputTextField.text = weight == 0 ? "" : String(format: "%.2f", weight)
+    }
+    
+    
+    @IBAction func didResetUserInfoButtonTapped(_ sender: UIButton) {
+        /// Alert 띄워서 어떤 정보를 없앨지 분기처리하거나,
+        /// 한번에 없앨거면 User struct 나중에 만드는 것으로.
+        UserDefaults.standard.removeObject(forKey: "nickname")
+        UserDefaults.standard.removeObject(forKey: "height")
+        UserDefaults.standard.removeObject(forKey: "weight")
+        loadUserInfo()
     }
 }
 
